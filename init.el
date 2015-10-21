2bae8f4cc02b50a3f6864f440399b1a9c7300adc
Ben Cooper <bkcooper@calicolabs.com>
2015-10-21 14:34:13 -0700

    reintroduce helm
    I turned back on the Helm stuff and I don't see any crashes.

--- init.el
+++ init.el
@@ -64,40 +64,40 @@
 (setq uniquify-buffer-name-style 'reverse)
 
 ; helm
-;(require 'helm)
-;(require 'helm-config)
+(require 'helm)
+(require 'helm-config)
 
-;(global-set-key (kbd "C-c h") 'helm-command-prefix)
-;(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
 
 ; some recommended keymappings
-;(global-set-key (kbd "M-x") 'helm-M-x)
-;(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-;(global-set-key (kbd "C-x b") 'helm-mini)
-;(global-set-key (kbd "C-x C-f") 'helm-find-files)
-;(global-set-key (kbd "C-c o") 'helm-occur)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c o") 'helm-occur)
 
-;(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
 ; rebind tab to run persistent action
-;(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
 ; make TAB works in terminal
-;(define-key helm-map (kbd "C-z")  'helm-select-action)
+(define-key helm-map (kbd "C-z")  'helm-select-action)
 ; list actions using C-z
 
 ; change some default executables for use in helm
-;(when (executable-find "curl")
-;  (setq helm-google-suggest-use-curl-p t))
-;(when (executable-find "ack-grep")
-;  (setq helm-grep-default-command "ack-grep -Hn --nogroup %p %f"
-;        helm-grep-default-recurse-command "ack-grep -H --nogroup %e %p %f"))
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+(when (executable-find "ack-grep")
+  (setq helm-grep-default-command "ack-grep -Hn --nogroup %p %f"
+        helm-grep-default-recurse-command "ack-grep -H --nogroup %e %p %f"))
 
-;(setq helm-split-window-in-side-p           t
-;      helm-move-to-line-cycle-in-source     t
-;      helm-ff-search-library-in-sexp        t
-;      helm-scroll-amount                    8
-;      helm-ff-file-name-history-use-recentf t)
+(setq helm-split-window-in-side-p           t
+      helm-move-to-line-cycle-in-source     t
+      helm-ff-search-library-in-sexp        t
+      helm-scroll-amount                    8
+      helm-ff-file-name-history-use-recentf t)
 
-;(helm-mode 1)
+(helm-mode 1)
 
 ;; Python configuration
 
