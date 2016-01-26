e2c82e6fc2e86e0e2c6130b0f7d72a4a4d3f7725
Ben Cooper <bkcooper@calicolabs.com>
2016-01-26 13:53:49 -0800

    Bind magit-status to C-x g

--- init.el
+++ init.el
@@ -40,6 +40,7 @@
 (global-set-key (kbd "M-/") 'hippie-expand) ; ropemode overrides
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
+(global-set-key (kdb "C-x g") 'magit-status)
 
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
